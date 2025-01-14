937250defc327ab8cd7310403f39581227f72d66
ibrahimsifat <ibsifat24@gmail.com>
2025-01-14 15:13:37 +0300

    UPDATED: repo

--- docker/phpmyadmin/config.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$cfg['ForceSSL'] = false;
-$cfg['PmaAbsoluteUri'] = 'https://ruchicart.com/phpmyadmin/';
-$cfg['AllowThirdPartyFraming'] = true;
-$cfg['CheckSSL'] = false;
-$cfg['VerifySSL'] = false;
