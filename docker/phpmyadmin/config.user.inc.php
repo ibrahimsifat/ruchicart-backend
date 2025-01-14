0b184f5f408bdf7ba5c43de0c64fb4179bc7921e
ibrahimsifat <ibsifat24@gmail.com>
2025-01-14 15:59:22 +0300

    UPDATED: phpmyadim

--- docker/phpmyadmin/config.user.inc.php
+++ docker/phpmyadmin/config.user.inc.php
@@ -13,3 +13,26 @@ $cfg['FirstLevelNavigationItems'] = 100;
 $cfg['AllowArbitraryServer'] = false;
 $cfg['LoginCookieValidity'] = 1440;
 $cfg['DefaultConnectionCollation'] = 'utf8mb4_unicode_ci';
+
+// SSL and Cookie Settings
+$cfg['ForceSSL'] = false;
+$cfg['PmaAbsoluteUri'] = 'https://ruchicart.com/phpmyadmin/';
+$cfg['Cookie_SameSite'] = 'Lax';
+$cfg['AuthLog'] = 'syslog';
+$cfg['TrustedProxies'] = ['nginx'];  // Trust the nginx proxy
+
+// Session and Cookie Security
+$cfg['LoginCookieDeleteAll'] = false;
+$cfg['LoginCookieStore'] = 0;
+$cfg['LoginCookieRecall'] = true;
+$cfg['CookieSameSite'] = 'Lax';
+$cfg['EnableAutocompleteForTablesAndColumns'] = true;
+
+// Error Handling
+$cfg['Error_Handler']['display'] = false;
+$cfg['Error_Handler']['gather'] = false;
+
+// Console Settings
+$cfg['Console']['StartHistory'] = false;
+$cfg['Console']['AlwaysExpand'] = false;
+$cfg['Console']['CurrentQuery'] = false;
