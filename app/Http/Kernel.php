b46cacdea9e23142e81385f12ae92c493bad4a4d
ibrahimsifat <ibsifat24@gmail.com>
2025-01-31 21:00:00 +0300

    UPDATED: send token for sign up

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,13 +24,14 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        // \App\Http\Middleware\Cors::class,
+        \App\Http\Middleware\Cors::class,
         // \Fruitcake\Cors\HandleCors::class
     ];
 
@@ -85,6 +86,6 @@ class Kernel extends HttpKernel
         'branch_status' => BranchStatusCheck::class,
         'branch_adder' => BranchAdder::class,
         'maintenance_mode' => MaintenanceModeMiddleware::class,
-        //  'cors' => \App\Http\Middleware\Cors::class,
+         'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
