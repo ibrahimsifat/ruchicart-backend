78d41f284a7d58323a588f8b62d201929f969553
ibrahimsifat <ibsifat24@gmail.com>
2025-01-31 21:33:30 +0300

    UPDATED: send token for sign up

--- app/Http/Controllers/Api/V1/Auth/CustomerAuthController.php
+++ app/Http/Controllers/Api/V1/Auth/CustomerAuthController.php
@@ -603,7 +603,7 @@ class CustomerAuthController extends Controller
         }
 
         $user = $this->user->where('phone', $responseData['phoneNumber'])->first();
-        
+
         if (isset($user)){
             if ($request['is_reset_token'] == 1){
                 DB::table('password_resets')->updateOrInsert(['email_or_phone' => $request->phoneNumber], [
@@ -620,7 +620,15 @@ class CustomerAuthController extends Controller
         }
         // replace tempToken to token for signup. when user signup they will login auto
         // $tempToken = Str::random(120);
-        return response()->json(['errors' => null, 'token' =>  $user->createToken('AuthToken')->accessToken], 200);
+        // return response()->json(['errors' => null, 'temp_token' => $tempToken], 200);
+
+        // new
+          // No need to create user - just generate token for new user signups
+        $token = $user->createToken('AuthToken')->accessToken;
+        $user->is_phone_verified = 1;
+        $user->save();
+        
+        return response()->json(['errors' => null,'token' => $token], 200);
     }
 
     public function verifyOTP(Request $request)
